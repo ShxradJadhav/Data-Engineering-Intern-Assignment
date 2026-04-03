8ab244a310a62c3a56678764691a21fecdaadd17
Sharad Jadhav <sharadjadhav1999@gmail.com>
2026-04-03 16:02:07 +0530

    Update clean_data.py

--- MyProject/api/clean_data.py
+++ MyProject/api/clean_data.py
@@ -3,7 +3,7 @@ import os
 
 # 1. Load the data
 # Using 'r' for raw string to avoid path errors on Windows
-path = r"C:/Users/User/Desktop/Web_Scrapper_project/jobs_fake_python.csv"
+path = "jobs_fake_python.csv"
 df = pd.read_csv(path)
 
 # 2. Remove exact duplicates
