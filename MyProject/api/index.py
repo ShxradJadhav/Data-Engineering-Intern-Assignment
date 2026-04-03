0ee6c9bed3f5cf538503e5995c6f1ebe85e79293
Sharad Jadhav <sharadjadhav1999@gmail.com>
2026-04-03 16:09:03 +0530

    Create index.py

---
+++ MyProject/api/index.py
@@ -0,0 +1,9 @@
+# index.py ke andar connection function
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 11557)) # Port 11557 zaroori hai
+    )
